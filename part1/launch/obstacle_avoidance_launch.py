02aa5c1cf498b0aae97c1c1b07b51f706076e026
Ajay <as3233@cam.ac.uk>
2022-11-01 15:10:45 +0000

    fix for Apple-M VM users for robot move, general fix for launch arg
    * change manager and controller update rates to allow short-circuiting control loop check
    ** modify yaml param file to apply only to robot0, breaks other namespacings if any
    * fix launch 'rule-based' from '--mode' to 'mode'

--- part1/launch/obstacle_avoidance_launch.py
+++ part1/launch/obstacle_avoidance_launch.py
@@ -5,7 +5,7 @@ from launch.substitutions import LaunchConfiguration, TextSubstitution
 
 
 def prepare_launch_nodes(context, *args, **kwargs):
-    mode = LaunchConfiguration('--mode', default="braitenberg").perform(context)
+    mode = LaunchConfiguration('mode', default="braitenberg").perform(context)
     ns = LaunchConfiguration('robot_name', default="robot0").perform(context)
     return [ExecuteProcess(
                 cmd=['ros2', 'run', 'part1', 'obstacle_avoidance', '--mode='+mode, '--ros-args', '--remap', '__ns:=/'+ns],
@@ -17,7 +17,7 @@ def generate_launch_description():
 
     return LaunchDescription([
         DeclareLaunchArgument('robot_name', default_value=TextSubstitution(text="robot0")),
-        DeclareLaunchArgument('--mode', default_value=TextSubstitution(text="braitenberg")),
+        DeclareLaunchArgument('mode', default_value=TextSubstitution(text="braitenberg")),
         GroupAction(
             actions=[PushRosNamespace(LaunchConfiguration('robot_name')),
                      OpaqueFunction(function=prepare_launch_nodes),
